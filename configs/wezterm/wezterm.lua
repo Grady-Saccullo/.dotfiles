5b7ade56a262a015e95134cba85c9cbcfc00f725
Grady Saccullo <gradys.dev@gmail.com>
2024-02-09 22:30:17 -0800

    tmux and wez confs

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require('wezterm')
 
 local config = {
-	color_scheme = "Catpuccin Mocha",
+	color_scheme = "Kanagawa (Gogh)",
 	enable_tab_bar = false,
 	macos_window_background_blur = 30,
-	window_background_opacity = 0.9,
+	window_background_opacity = 1,
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 0,
+		bottom = 0,
+	},
+	font = wezterm.font("JetBrains Mono", { weight = "Book" }),
 }
 
 -- if wezterm.config_builder then
@@ -12,3 +19,4 @@ local config = {
 -- end
 
 return config
+
