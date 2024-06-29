81b65ec44217da3502f4d71a0cc0912af607a59a
Grady Saccullo <gradys.dev@gmail.com>
2024-06-29 08:58:09 -0700

    wip: moving part of nvim to nix

--- modules/shared/programs/nvim.nix
+++ modules/shared/programs/nvim.nix
@@ -4,21 +4,46 @@
   ...
 }: let
   configDir = "${config.home.homeDirectory}/.dotfiles/configs/nvim/.config/nvim";
+
+  tresitterPlugins = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+        p.bash
+        p.dockerfile
+        p.go
+        p.html
+        p.json
+        p.lua
+        p.nix
+        p.python
+        p.swift
+        p.tsx
+        p.typescript
+  ]));
+
+  treesitterParsers = pkgs.symlinkJoin {
+    name = "treesitter-parsers";
+    paths = tresitterPlugins.dependencies;
+  };
 in {
-  xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink configDir;
-    recursive = true;
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink configDir;
+  home.file.".dotfiles/configs/nvim/.config/nvim/lua/configs/nix.lua".text = ''
+    vim.opt.runtimepath:append("${treesitterParsers}")
+  '';
+
+  home.file.".local/share/nvim/nix/nvim-treesitter/" = {
+      source = tresitterPlugins;
+      recursive = true;
   };
 
-  programs = {
-    neovim = {
+  programs.neovim = {
       enable = true;
       package = pkgs.unstable.neovim-unwrapped;
 
-      withPython3 = true;
-
-      viAlias = true;
+      defaultEditor = true;
+      withNodeJs = true;
       vimAlias = true;
-    };
+
+      plugins = [
+        tresitterPlugins
+      ];
   };
 }
