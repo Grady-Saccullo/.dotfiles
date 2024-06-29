4dd39c8fc3099edca0f5b215bf6dde12a2fcee2a
Hackerman <gradys.dev@gmail.com>
2024-06-29 21:16:24 +0000

    wip: moved treesitter to nix

--- modules/shared/programs/nvim.nix
+++ modules/shared/programs/nvim.nix
@@ -5,45 +5,66 @@
 }: let
   configDir = "${config.home.homeDirectory}/.dotfiles/configs/nvim/.config/nvim";
 
-  tresitterPlugins = (pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
-        p.bash
-        p.dockerfile
-        p.go
-        p.html
-        p.json
-        p.lua
-        p.nix
-        p.python
-        p.swift
-        p.tsx
-        p.typescript
-  ]));
+  treesitterPlugins = pkgs.vimPlugins.nvim-treesitter.withPlugins (p: [
+    p.bash
+    p.dockerfile
+    p.go
+    p.css
+    p.html
+    p.json
+    p.lua
+    p.nix
+    p.ocaml
+    p.python
+    p.rust
+    p.swift
+    p.tsx
+    p.typescript
+  ]);
 
   treesitterParsers = pkgs.symlinkJoin {
     name = "treesitter-parsers";
-    paths = tresitterPlugins.dependencies;
+    paths = treesitterPlugins.dependencies;
   };
 in {
   home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink configDir;
+
   home.file.".dotfiles/configs/nvim/.config/nvim/lua/configs/nix.lua".text = ''
+    vim.g.treesitter_parsers_path = "${treesitterParsers}"
     vim.opt.runtimepath:append("${treesitterParsers}")
   '';
 
-  home.file.".local/share/nvim/nix/nvim-treesitter/" = {
-      source = tresitterPlugins;
-      recursive = true;
+  home.file.".local/share/nvim/nix/nvim-treesitter" = {
+    source = treesitterPlugins;
+    recursive = true;
   };
 
   programs.neovim = {
-      enable = true;
-      package = pkgs.unstable.neovim-unwrapped;
+    enable = true;
+    package = pkgs.unstable.neovim-unwrapped;
 
-      defaultEditor = true;
-      withNodeJs = true;
-      vimAlias = true;
+    defaultEditor = true;
+    withNodeJs = true;
+    vimAlias = true;
 
-      plugins = [
-        tresitterPlugins
-      ];
+    plugins = [
+      treesitterPlugins
+    ];
+
+    extraPackages = with pkgs.unstable; [
+      # lsp made available only to nvim
+      gopls
+      htmx-lsp
+      lua-language-server
+      nil
+      ocamlPackages.lsp
+      pyright
+      rust-analyzer
+      sourcekit-lsp
+      sqls
+      templ
+      vscode-langservers-extracted
+      zls
+    ];
   };
 }
