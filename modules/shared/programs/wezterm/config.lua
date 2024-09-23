b360b831596d198fb97d5a09c543b7868587b0da
Grady Saccullo <gradys.dev@gmail.com>
2024-09-22 17:56:25 -0700

    refactor: wezterm colors

--- modules/shared/programs/wezterm/config.lua
+++ modules/shared/programs/wezterm/config.lua
@@ -1,7 +1,7 @@
 local wezterm = require('wezterm')
 
 local config = {
-	color_scheme = "Oxocarbon Dark",
+	color_scheme = "oxocarbon-dark",
 	enable_tab_bar = false,
 	font = wezterm.font("JetBrains Mono", { weight = "Book" }),
 	window_close_confirmation = 'NeverPrompt',
