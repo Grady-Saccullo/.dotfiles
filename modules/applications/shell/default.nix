d4f7a22d2e22cec6fb59bf3a5e1b764fdb9e8a73
Grady Saccullo <gradys.dev@gmail.com>
2025-04-01 20:32:11 -0700

    fix: fzf layout and opening in vim

--- modules/applications/shell/default.nix
+++ modules/applications/shell/default.nix
@@ -23,11 +23,66 @@ in {
       btop.enable = true;
       jq.enable = true;
 
-      fzf = lib.mkIf cfg.fzf.enable {
+      fzf = lib.mkIf cfg.fzf.enable (let
+        baseOptions = [
+          "--height 100%"
+          "--layout default"
+          "--border"
+        ];
+        fileOptions =
+          baseOptions
+          ++ [
+            "--preview 'bat --color=always {}'"
+            "--bind 'enter:become(vim {} < /dev/tty > /dev/tty)'"
+          ];
+      in {
         enable = true;
         package = pkgs.unstable.fzf;
         enableZshIntegration = cfg.zsh.enable;
+        defaultCommand = "fd -t f -H . $HOME";
+        fileWidgetCommand = "fd -t f -H . $HOME";
+        fileWidgetOptions = fileOptions;
+        changeDirWidgetCommand = "fd -t d -H . $HOME";
+        changeDirWidgetOptions = baseOptions;
         tmux.enableShellIntegration = cfg.tmux.enable;
+      });
+
+      fd = lib.mkIf cfg.fzf.enable {
+        enable = true;
+        ignores = [
+          ".aws/"
+          ".bash_sessions/"
+          ".cache/"
+          ".cargo/"
+          ".config/"
+          ".gem/"
+          ".git/"
+          ".local/"
+          ".matplotlib/"
+          ".nix-defexpr/"
+          ".nix-profile/"
+          ".npm/"
+          ".rustup/"
+          ".ssh/"
+          ".tmux/"
+          ".Trash/"
+          ".vim/"
+          ".zsh_sessions/"
+          "Applications/"
+          "bin/"
+          "build/"
+          "dist/"
+          "go/"
+          "Library/"
+          "Movies/"
+          "Music/"
+          "node_modules/"
+          "out/"
+          "Pictures/"
+          "Public/"
+          "target/"
+          "vendor/"
+        ];
       };
 
       zsh = lib.mkIf cfg.zsh.enable {
