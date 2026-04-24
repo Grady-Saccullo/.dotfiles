1fa6ba66a0fc854dd51007b8357c5bc5fa49d181
Grady Saccullo <gradys.dev@gmail.com>
2026-04-24 08:09:12 -0700

    update areospace app detection

--- configurations/personal-configs/aerospace.nix
+++ configurations/personal-configs/aerospace.nix
@@ -5,8 +5,16 @@ in
   // {
     on-window-detected = [
       {
+        "if" = {app-id = "com.github.wez.wezterm";};
+        run = "move-node-to-workspace 1";
+      }
+      {
+        "if" = {app-id = "com.brave.Browser";};
+        run = "move-node-to-workspace 2";
+      }
+      {
         "if" = {app-id = "com.spotify.client";};
-        run = "move-node-to-workspace 3";
+        run = "move-node-to-workspace 4";
       }
     ];
   }
--- configurations/voze-configs/aerospace.nix
+++ configurations/voze-configs/aerospace.nix
@@ -5,6 +5,10 @@ in
   // {
     on-window-detected = [
       {
+        "if" = {app-id = "com.github.wez.wezterm";};
+        run = "move-node-to-workspace 1";
+      }
+      {
         "if" = {app-id = "com.jetbrains.intellij";};
         run = "move-node-to-workspace 1";
       }
@@ -13,24 +17,24 @@ in
         run = "move-node-to-workspace 1";
       }
       {
-        "if" = {app-id = "com.github.wez.wezterm";};
+        "if" = {app-id = "com.brave.Browser";};
         run = "move-node-to-workspace 2";
       }
       {
-        "if" = {app-id = "com.brave.Browser";};
-        run = "move-node-to-workspace 3";
-      }
-      {
         "if" = {app-id = "us.zoom.xos";};
-        run = ["move-node-to-workspace 4" "layout floating"];
+        run = ["move-node-to-workspace 3" "layout floating"];
       }
       {
         "if" = {app-id = "com.tinyspeck.slackmacgap";};
-        run = "move-node-to-workspace 5";
+        run = "move-node-to-workspace 4";
       }
       {
         "if" = {app-id = "com.hnc.Discord";};
-        run = "move-node-to-workspace 5";
+        run = "move-node-to-workspace 4";
+      }
+      {
+        "if" = {app-id = "com.spotify.client";};
+        run = "move-node-to-workspace 6";
       }
     ];
   }
